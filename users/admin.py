f8c69a66b8215079dc39893fd74538d144d422da
Moritz Ellmers <moritz.ellmers@web.de>
2020-01-26 03:03:28 +0100

    added admin model for profile

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio', 'birth_date', 'city', 'country', 'clan', 'games', 'gamertag', 'logo', 'updated_at', 'created_at')
+    list_display_links = ('id', 'user')
+    list_filter = ('clan', 'city', 'country')
+    search_fields = ('bio', 'birth_date', 'city', 'country', 'clan', 'games', 'gamertag')
+
+
+admin.site.register(Profile, ProfileAdmin)
